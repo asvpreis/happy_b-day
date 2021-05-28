a770a6398740928b88fe4fb30d4069b8359db4ac
Anna Preis <anna.preis@shopify.com>
2021-05-28 13:08:15 +0100

    Complete part 1

---
+++ happy_b-day.rb
@@ -0,0 +1,8 @@
+# Ask user for the name
+print "What is your name? "
+user_name = gets.chomp
+
+# output Happy birthday
+2.times {puts "Happy birthday to you"}
+puts "Happy birthday dear #{user_name}"
+puts "Happy birthday to you"
